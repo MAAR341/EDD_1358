7ef8dfda514a8c3dcfd91dc043ff9a1cc69e997f
MAAR341 <89878823+MAAR341@users.noreply.github.com>
2022-10-14 11:44:50 -0500

    Laberinto

---
+++ NodoDoble.java
@@ -0,0 +1,55 @@
+
+package com.mycompany.array;
+
+public class NodoDoble<T> {
+    private T dato;
+    private NodoDoble<T> siguiente;
+    private NodoDoble<T> anterior;
+    
+    public NodoDoble(){
+        
+    }
+    
+    public NodoDoble (T valor) {
+        this.dato = valor;
+    }
+
+    public NodoDoble (T dato, NodoDoble<T> anterior, NodoDoble<T> siguiente) {
+        this.dato = dato;
+        this.siguiente = siguiente;
+        this.anterior = anterior;
+    }
+
+    public NodoDoble<T> getSiguiente() {
+        return siguiente;
+    }
+    
+    public NodoDoble<T> getAnterior(){
+        return anterior;
+    }
+
+    public void setSiguiente(NodoDoble<T> siguiente) {
+        this.siguiente = siguiente;
+    }
+    
+    public void setAnterior(NodoDoble<T> anterior) {
+        this.anterior = anterior;
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    @Override
+    public String toString() {
+        
+        return "-> |"+dato; 
+    }
+    public String printAll(){
+        return "[Anterior : " + this.getAnterior() + " <- [Dato = " + dato + "] -> [Siguiente : " + this.getSiguiente() + "]";
+    }
+}
